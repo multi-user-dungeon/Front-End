e6880a1181fa0c69e579e3ab4e1f8a845716e379
Nicholas Pung <nckpng95@gmail.com>
2019-10-23 08:22:21 -0700

    Rooms object now has coordinates

--- src/components/Map.js
+++ src/components/Map.js
@@ -13,26 +13,35 @@ const Map = ({ roomsArray, roomsObject }) => {
         let box = roomsObject[room].fields;
         let findXandY = "";
         if (box.e_to) {
-          if (typeof roomsObject[box.e_to].fields.x === 'number') {
+          if (typeof roomsObject[box.e_to].fields.x === "number") {
             findXandY = "e_to";
+            box.x = roomsObject[box[findXandY]].fields.x - 1;
+            box.y = roomsObject[box[findXandY]].fields.y;
           }
         }
         if (box.w_to) {
-          if (typeof roomsObject[box.w_to].fields.x === 'number') {
+          if (typeof roomsObject[box.w_to].fields.x === "number") {
             findXandY = "w_to";
+            box.x = roomsObject[box[findXandY]].fields.x + 1;
+            box.y = roomsObject[box[findXandY]].fields.y;
           }
         }
         if (box.n_to) {
-          if (typeof roomsObject[box.n_to].fields.x === 'number') {
+          if (typeof roomsObject[box.n_to].fields.x === "number") {
             findXandY = "n_to";
+            box.x = roomsObject[box[findXandY]].fields.x;
+            box.y = roomsObject[box[findXandY]].fields.y - 1;
           }
         }
         if (box.s_to) {
-          if (typeof roomsObject[box.s_to].fields.x === 'number') {
+          if (typeof roomsObject[box.s_to].fields.x === "number") {
             findXandY = "s_to";
+            box.x = roomsObject[box[findXandY]].fields.x;
+            box.y = roomsObject[box[findXandY]].fields.y + 1;
           }
         }
-        console.log(roomsObject[box[findXandY]].fields)
+
+        console.log(box.x, box.y);
       }
     });
     console.log(roomsObject);
