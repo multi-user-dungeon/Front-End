e0afab2e61fc4488cad2efc591ad36702350794d
Nicholas Pung <nckpng95@gmail.com>
2019-10-23 08:35:18 -0700

    Was able to return the correct coordinates

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,46 +5,47 @@ const Map = ({ roomsArray, roomsObject }) => {
   const createCoordinatesFromFirstPoint = () => {
     roomsObject[1].fields.x = 0;
     roomsObject[1].fields.y = 0;
-    // console.log(roomsObject[roomsObject[2].fields.s_to].fields.x)
+    let coordinates = {};
+    coordinates[1] = roomsObject[1].fields;
     Object.keys(roomsObject).forEach(room => {
-      if (room.x === 0 && room.y === 0) {
-        return;
-      } else {
-        let box = roomsObject[room].fields;
-        let findXandY = "";
-        if (box.e_to) {
-          if (typeof roomsObject[box.e_to].fields.x === "number") {
-            findXandY = "e_to";
-            box.x = roomsObject[box[findXandY]].fields.x - 1;
-            box.y = roomsObject[box[findXandY]].fields.y;
-          }
+      let box = roomsObject[room].fields;
+      let findXandY = "";
+      if (box.e_to) {
+        if (typeof roomsObject[box.e_to].fields.x === "number") {
+          findXandY = "e_to";
+          box.x = roomsObject[box[findXandY]].fields.x - 1;
+          box.y = roomsObject[box[findXandY]].fields.y;
+          coordinates[room] = roomsObject[room].fields;
         }
-        if (box.w_to) {
-          if (typeof roomsObject[box.w_to].fields.x === "number") {
-            findXandY = "w_to";
-            box.x = roomsObject[box[findXandY]].fields.x + 1;
-            box.y = roomsObject[box[findXandY]].fields.y;
-          }
-        }
-        if (box.n_to) {
-          if (typeof roomsObject[box.n_to].fields.x === "number") {
-            findXandY = "n_to";
-            box.x = roomsObject[box[findXandY]].fields.x;
-            box.y = roomsObject[box[findXandY]].fields.y - 1;
-          }
-        }
-        if (box.s_to) {
-          if (typeof roomsObject[box.s_to].fields.x === "number") {
-            findXandY = "s_to";
-            box.x = roomsObject[box[findXandY]].fields.x;
-            box.y = roomsObject[box[findXandY]].fields.y + 1;
-          }
-        }
-
-        console.log(box.x, box.y);
       }
+      if (box.w_to) {
+        if (typeof roomsObject[box.w_to].fields.x === "number") {
+          findXandY = "w_to";
+          box.x = roomsObject[box[findXandY]].fields.x + 1;
+          box.y = roomsObject[box[findXandY]].fields.y;
+          coordinates[room] = roomsObject[room].fields;
+        }
+      }
+      if (box.n_to) {
+        if (typeof roomsObject[box.n_to].fields.x === "number") {
+          findXandY = "n_to";
+          box.x = roomsObject[box[findXandY]].fields.x;
+          box.y = roomsObject[box[findXandY]].fields.y - 1;
+          coordinates[room] = roomsObject[room].fields;
+        }
+      }
+      if (box.s_to) {
+        if (typeof roomsObject[box.s_to].fields.x === "number") {
+          findXandY = "s_to";
+          box.x = roomsObject[box[findXandY]].fields.x;
+          box.y = roomsObject[box[findXandY]].fields.y + 1;
+          coordinates[room] = roomsObject[room].fields;
+        }
+      }
+
+      console.log(box.x, box.y);
     });
-    console.log(roomsObject);
+    console.log(coordinates);
   };
 
   if (roomsArray[0]) {
