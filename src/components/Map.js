ec07869be8aca09dc0ce6331eb253f2d42092e78
Nicholas Pung <nckpng95@gmail.com>
2019-10-23 09:07:24 -0700

    Exported the function out of component to its own thing

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,55 +1,11 @@
 import React from "react";
 import Room from "./Room";
+import createCoordinatesFromFirstPoint from "../utils/createCoordinatesFromFirstPoint";
 
 const Map = ({ roomsArray, roomsObject }) => {
-  const createCoordinatesFromFirstPoint = () => {
-    roomsObject[1].fields.x = 0;
-    roomsObject[1].fields.y = 0;
-    let coordinates = {};
-    coordinates[1] = roomsObject[1].fields;
-    Object.keys(roomsObject).forEach(room => {
-      let box = roomsObject[room].fields;
-      let findXandY = "";
-      if (box.e_to) {
-        if (typeof roomsObject[box.e_to].fields.x === "number") {
-          findXandY = "e_to";
-          box.x = roomsObject[box[findXandY]].fields.x - 1;
-          box.y = roomsObject[box[findXandY]].fields.y;
-          coordinates[room] = roomsObject[room].fields;
-        }
-      }
-      if (box.w_to) {
-        if (typeof roomsObject[box.w_to].fields.x === "number") {
-          findXandY = "w_to";
-          box.x = roomsObject[box[findXandY]].fields.x + 1;
-          box.y = roomsObject[box[findXandY]].fields.y;
-          coordinates[room] = roomsObject[room].fields;
-        }
-      }
-      if (box.n_to) {
-        if (typeof roomsObject[box.n_to].fields.x === "number") {
-          findXandY = "n_to";
-          box.x = roomsObject[box[findXandY]].fields.x;
-          box.y = roomsObject[box[findXandY]].fields.y - 1;
-          coordinates[room] = roomsObject[room].fields;
-        }
-      }
-      if (box.s_to) {
-        if (typeof roomsObject[box.s_to].fields.x === "number") {
-          findXandY = "s_to";
-          box.x = roomsObject[box[findXandY]].fields.x;
-          box.y = roomsObject[box[findXandY]].fields.y + 1;
-          coordinates[room] = roomsObject[room].fields;
-        }
-      }
-
-      console.log(box.x, box.y);
-    });
-    console.log(coordinates);
-  };
-
   if (roomsArray[0]) {
-    createCoordinatesFromFirstPoint();
+    const coordinates = createCoordinatesFromFirstPoint(roomsObject);
+    console.log(coordinates);
   }
 
   return (
---
+++ src/utils/createCoordinatesFromFirstPoint.js
@@ -0,0 +1,45 @@
+const createCoordinatesFromFirstPoint = roomsObject => {
+  roomsObject[1].fields.x = 0;
+  roomsObject[1].fields.y = 0;
+  let coordinates = {};
+  coordinates[1] = roomsObject[1].fields;
+  Object.keys(roomsObject).forEach(room => {
+    let box = roomsObject[room].fields;
+    let findXandY = "";
+    if (box.e_to) {
+      if (typeof roomsObject[box.e_to].fields.x === "number") {
+        findXandY = "e_to";
+        box.x = roomsObject[box[findXandY]].fields.x - 1;
+        box.y = roomsObject[box[findXandY]].fields.y;
+        coordinates[room] = roomsObject[room].fields;
+      }
+    }
+    if (box.w_to) {
+      if (typeof roomsObject[box.w_to].fields.x === "number") {
+        findXandY = "w_to";
+        box.x = roomsObject[box[findXandY]].fields.x + 1;
+        box.y = roomsObject[box[findXandY]].fields.y;
+        coordinates[room] = roomsObject[room].fields;
+      }
+    }
+    if (box.n_to) {
+      if (typeof roomsObject[box.n_to].fields.x === "number") {
+        findXandY = "n_to";
+        box.x = roomsObject[box[findXandY]].fields.x;
+        box.y = roomsObject[box[findXandY]].fields.y - 1;
+        coordinates[room] = roomsObject[room].fields;
+      }
+    }
+    if (box.s_to) {
+      if (typeof roomsObject[box.s_to].fields.x === "number") {
+        findXandY = "s_to";
+        box.x = roomsObject[box[findXandY]].fields.x;
+        box.y = roomsObject[box[findXandY]].fields.y + 1;
+        coordinates[room] = roomsObject[room].fields;
+      }
+    }
+  });
+  return coordinates;
+};
+
+export default createCoordinatesFromFirstPoint;
